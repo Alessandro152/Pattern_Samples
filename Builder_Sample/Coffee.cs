8f3c0033be0f03b86d5e1ba188628a455190b6d7
alessandro.sanzo <alessandro.sanzo@totvs.com.br>
2022-12-02 14:23:49 -0300

    primeiro commit

---
+++ Builder_Sample/Coffee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Builder_Sample
+{
+    public class Coffee
+    {
+        public Coffee()
+        {
+
+        }
+
+        public string Type { get; set; }
+
+        public bool Suggar { get; set; }
+    }
+}
---
+++ Builder_Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Builder_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coffee = CoffeeBuilder.Start(new Coffee())
+                                      .Build("expresso");
+
+            Console.WriteLine($"Your coffee has been done!!");
+            Console.ReadKey();
+        }
+    }
+}
