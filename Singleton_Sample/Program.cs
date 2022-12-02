034cc97418be31d84fd8b743a7a8429668cb9c2f
alessandro.sanzo <alessandro.sanzo@totvs.com.br>
2022-12-02 14:46:45 -0300

    publicacao exemplo singleton

---
+++ Singleton_Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Singleton_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Singleton.GetInstance();
+            Console.WriteLine(Singleton.Instancia);
+
+            Singleton.GetInstance();
+            Console.WriteLine(Singleton.Instancia);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton_Sample/Singleton.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton_Sample
+{
+    public sealed class Singleton
+    {
+        private static Singleton _singleton;
+
+        public static string Instancia { get; set; }
+
+        public static Singleton GetInstance()
+        {
+            if(_singleton is null)
+            {
+                _singleton = new Singleton();
+                Instancia = "criando nova instancia singleton";
+
+                return _singleton;
+            }
+
+            Instancia = "utilizando mesma instancia singleton";
+            return _singleton;
+        }
+    }
+}
